be5911330723af36260dd3a36e03d1aa42f7d987
ElyseMB <135876433+ElyseMB@users.noreply.github.com>
2023-06-07 15:10:41 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
-basic.forever(function () {
+input.onButtonPressed(Button.A, function () {
 	
 })
+input.onButtonPressed(Button.AB, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+input.onGesture(Gesture.Shake, function () {
+	
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+	
+})
+radio.setGroup(3)
+radio.setFrequencyBand(33)
+basic.showIcon(IconNames.Sad)
+music.playSoundEffect(music.builtinSoundEffect(soundExpression.hello), SoundExpressionPlayMode.UntilDone)
