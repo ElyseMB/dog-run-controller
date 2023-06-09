f6b5fb5bb3afa42c2c774359c0b8d415717a68f8
ElyseMB <135876433+ElyseMB@users.noreply.github.com>
2023-06-09 14:25:21 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,21 @@
 input.onButtonPressed(Button.A, function () {
-	
+    radio.sendNumber(1)
 })
 input.onButtonPressed(Button.AB, function () {
-	
+    radio.sendNumber(3)
 })
 input.onButtonPressed(Button.B, function () {
-	
+    radio.sendNumber(2)
 })
 input.onGesture(Gesture.Shake, function () {
-	
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.Vibrato, InterpolationCurve.Logarithmic), SoundExpressionPlayMode.UntilDone)
+    radio.sendNumber(5)
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-	
+    radio.sendNumber(4)
 })
 radio.setGroup(3)
 radio.setFrequencyBand(33)
 basic.showIcon(IconNames.Sad)
 music.playSoundEffect(music.builtinSoundEffect(soundExpression.hello), SoundExpressionPlayMode.UntilDone)
+radio.sendNumber(0)
