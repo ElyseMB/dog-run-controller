845ae0337566b6e304b03c2615b48c8ee7c10c49
ElyseMB <135876433+ElyseMB@users.noreply.github.com>
2023-06-16 12:53:00 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,22 @@
 input.onButtonPressed(Button.A, function () {
     radio.sendNumber(1)
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Noise, 5000, 0, 255, 0, 500, SoundExpressionEffect.Vibrato, InterpolationCurve.Logarithmic), SoundExpressionPlayMode.UntilDone)
 })
 input.onButtonPressed(Button.AB, function () {
     radio.sendNumber(3)
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Noise, 5000, 0, 255, 0, 500, SoundExpressionEffect.Vibrato, InterpolationCurve.Logarithmic), SoundExpressionPlayMode.UntilDone)
 })
 input.onButtonPressed(Button.B, function () {
     radio.sendNumber(2)
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Noise, 5000, 0, 255, 0, 500, SoundExpressionEffect.Vibrato, InterpolationCurve.Logarithmic), SoundExpressionPlayMode.UntilDone)
 })
 input.onGesture(Gesture.Shake, function () {
-    music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.Vibrato, InterpolationCurve.Logarithmic), SoundExpressionPlayMode.UntilDone)
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Noise, 5000, 0, 255, 0, 500, SoundExpressionEffect.Vibrato, InterpolationCurve.Logarithmic), SoundExpressionPlayMode.UntilDone)
     radio.sendNumber(5)
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     radio.sendNumber(4)
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Noise, 5000, 0, 255, 0, 500, SoundExpressionEffect.Vibrato, InterpolationCurve.Logarithmic), SoundExpressionPlayMode.UntilDone)
 })
 radio.setGroup(3)
 radio.setFrequencyBand(33)
